9196312fb0ddd3d1aa618c77e414c2584bea32b8
SobuHasy <sobu.hasy@hotmail.com>
2024-01-29 16:58:51 +0100

    Iterated over each problem by splitting into operands and operator and calculated the width, then lines are constructed

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -2,3 +2,12 @@ def arithmetic_arranger(problems):
     line1 = ""
     line2 = ""
     line3 = ""
+
+    for problem in problems:
+        operand1, operator, operand2 = problem.split()
+
+        width = max(len(operand1), len(operand2)) + 2
+
+        line1 += operand1.rjust(width) + " "
+        line2 += operator + " " + operand2.rjust(width - 2) * " "
+        line3 += "-" * width + ""
